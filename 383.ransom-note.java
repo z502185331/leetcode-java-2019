df185c768eb6825dbceeb417762c83b5f59b41fb
z502185331 <zoulieyong@gmail.com>
2021-02-20 16:10:36 -0800

    Update 383.ransom-note.java

--- 383.ransom-note.java
+++ 383.ransom-note.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
+    public boolean canConstruct1(String ransomNote, String magazine) {
         Map<Character, Integer> count = new HashMap<>();
         for (char c : magazine.toCharArray()) {
             if (!count.containsKey(c)) {
@@ -19,4 +19,21 @@ class Solution {
         
         return true;
     }
-}383
+    
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int counts[] = new int[26]; 
+        for (char c : magazine.toCharArray()) {
+            counts[(int)c - (int)'a'] += 1;    
+        }
+        
+        for (char c: ransomNote.toCharArray()) {
+            counts[(int)c - (int)'a'] -= 1;
+            
+            if (counts[(int)c - (int)'a'] < 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
